1d54fdd94129471258eeb885d429b2471eded5ce
Tahsin Özgür KOÇ <tahsinkoc6@gmail.com>
2023-12-06 19:39:54 +0300

    The Authentication function changed as a component

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import User from './schemas/UserScheme.js';
 import Encrypt from './comps/Cryptor.js';
 import Unicity from './comps/Unicity.js';
+import AuthenticateToken from './comps/Authenticate.js'
 
 const app = express();
 
@@ -21,29 +22,7 @@ app.use(cors())
 
 const KEY = 'a1b9c961-b14a-4ed7-8724-70a36d3146bb';
 
-const AuthenticateToken = (req, res, next, acceptableRoles) => {
 
-    const token = req.header("Auth");
-
-    if (!token) {
-        // console.log(token)
-        return res.status(401).send({ message: 'Acces denied. Token is required' });
-    }
-    // console.log(token)
-    jwt.verify(token, KEY, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Invalid Token.' });
-        } else {
-            const RoleCheck = acceptableRoles.find((item) => item == decoded.role);
-            if (!RoleCheck) {
-                return res.status(403).send({ message: 'You don`t have a permision to acces here.' });
-            }
-            req.user = decoded;
-            next();
-        }
-    })
-
-}
 
 
 
